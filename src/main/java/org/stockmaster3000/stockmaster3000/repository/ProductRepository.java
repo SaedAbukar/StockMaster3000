7298db0e3258290abaf7ce190091571c49aac689
viettranni <vttranviett@gmail.com>
2025-02-04 12:34:22 +0200

    Adding more tasks for myself

--- src/main/java/org/stockmaster3000/stockmaster3000/repository/ProductRepository.java
+++ src/main/java/org/stockmaster3000/stockmaster3000/repository/ProductRepository.java
@@ -14,12 +14,6 @@ import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
-    // Updating the Product Quantity
-    @Modifying
-    @Transactional
-    @Query("UPDATE Product p SET p.quantity = :quantity WHERE p.id = :productId")
-    int updateProductQuantity(@Param("productId") Long productId, @Param("quantity") int quantity);
 
     // Finding Products by Inventory ID
     List<Product> findByInventoryId(Long inventoryId);
@@ -27,4 +21,17 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // Returning the list of Products which are expiring soon
     @Query("SELECT p FROM Product p WHERE p.amountOfDaysUntilExpiration <= 3")
     List<Product> getExpiringSoonProducts();
+
+    // Return a list of Products which is low on stock
+
+
+    // Return a list of Products which is out of stock
+
+
+    // Update the Product Quantity
+    @Modifying
+    @Transactional
+    @Query("UPDATE Product p SET p.quantity = :quantity WHERE p.id = :productId")
+    int updateProductQuantity(@Param("productId") Long productId, @Param("quantity") int quantity);
+
 }
