f911e59dc9a47e33cbfac2cf92072d26d631d426
pawaffle <pdegterev@gmail.com>
2025-02-07 12:39:14 +0200

    test commit before the merge

---
+++ src/main/frontend/generated/flow/chunks/chunk-6502649f9be867d8d7f84fd6b816f9b0b6b567674ba7af516296aeb7ce505744.js
@@ -0,0 +1,6 @@
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/app-layout/src/vaadin-app-layout.js';
+import '@vaadin/tooltip/src/vaadin-tooltip.js';
+import '@vaadin/horizontal-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/button/src/vaadin-button.js';
+import 'Frontend/generated/jar-resources/disableOnClickFunctions.js';
