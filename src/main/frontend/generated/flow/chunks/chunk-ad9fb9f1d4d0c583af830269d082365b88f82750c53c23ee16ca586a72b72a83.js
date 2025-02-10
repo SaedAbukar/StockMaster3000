a8f4dd797f04af946612c157de56b716d6bd1d4c
BudaOP <ibudanov24@gmail.com>
2025-02-10 23:15:15 +0200

    Fixed Pom file (removed extra maven plugin)

---
+++ src/main/frontend/generated/flow/chunks/chunk-ad9fb9f1d4d0c583af830269d082365b88f82750c53c23ee16ca586a72b72a83.js
@@ -0,0 +1,6 @@
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/vertical-layout/theme/lumo/vaadin-vertical-layout.js';
+import '@vaadin/tooltip/theme/lumo/vaadin-tooltip.js';
+import '@vaadin/horizontal-layout/theme/lumo/vaadin-horizontal-layout.js';
+import '@vaadin/button/theme/lumo/vaadin-button.js';
+import 'Frontend/generated/jar-resources/disableOnClickFunctions.js';
