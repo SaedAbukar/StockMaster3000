d91c9c9d45486c87b530b81d283dba301068872e
BudaOP <ibudanov24@gmail.com>
2025-02-11 19:46:35 +0200

    Modified and enchanced some tests, Added HeaderComponentTest (96% DONE in Jacoco, now excluded till next sprint)

--- src/test/java/org/stockmaster3000/TestDataUtil.java
+++ src/test/java/org/stockmaster3000/TestDataUtil.java
@@ -2,29 +2,33 @@ package org.stockmaster3000;
 
 import org.stockmaster3000.stockmaster3000.model.User;
 
+// Utility class for creating test user objects
 public final class TestDataUtil {
     private TestDataUtil() {
     }
 
+    // Creates and returns a test user with predefined values
     public static User createTestUserA() {
         User user = new User();
-        user.setId(1L);
-        user.setUsername("test1");
-        user.setPassword("12345678");
+        user.setId(1L); // Assign a unique ID to user A
+        user.setUsername("test1"); // Set username
+        user.setPassword("12345678"); // Set password
         return user;
     }
 
+    // Creates and returns another test user with different values
     public static User createTestUserB() {
         User user = new User();
-        user.setId(2L); // Changed id to be unique
+        user.setId(2L); // Assign a unique ID to user B
         user.setUsername("test2");
         user.setPassword("12345678");
         return user;
     }
 
+    // Creates and returns a third test user with different values
     public static User createTestUserC() {
         User user = new User();
-        user.setId(3L); // Changed id to be unique
+        user.setId(3L); // Assign a unique ID to user C
         user.setUsername("test3");
         user.setPassword("12345678");
         return user;
