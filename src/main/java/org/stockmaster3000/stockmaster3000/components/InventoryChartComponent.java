653e99b2298b3550456dc29f62a7c0a44fdd4e27
viettranni <vttranviett@gmail.com>
2025-02-13 22:11:31 +0200

    Adding initial data into doughnut chart for it to display when there is no data

--- src/main/java/org/stockmaster3000/stockmaster3000/components/InventoryChartComponent.java
+++ src/main/java/org/stockmaster3000/stockmaster3000/components/InventoryChartComponent.java
@@ -8,6 +8,8 @@ import org.stockmaster3000.stockmaster3000.service.ProductService;
 import org.stockmaster3000.stockmaster3000.service.InventoryService;
 import org.stockmaster3000.stockmaster3000.security.SecurityService;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
 
 public class InventoryChartComponent extends VerticalLayout {
 
@@ -21,8 +23,15 @@ public class InventoryChartComponent extends VerticalLayout {
         this.productService = productService;
         this.inventoryService = inventoryService;
 
+        H1 componentTopic = new H1("Inventory Insights");
+        add(componentTopic);
+
+        // Add a subtitle for the doughnut chart
+        H3 doughnutTopic = new H3("Distribution of Inventory Categories");
+        add(doughnutTopic);
+
         // Initialize the doughnut chart with empty data
-        doughnutChart = new DoughnutChart(new String[]{}, new int[]{});
+        doughnutChart = new DoughnutChart(new String[]{"No Data"}, new int[]{1});
         add(doughnutChart);
     }
 
@@ -47,7 +56,7 @@ public class InventoryChartComponent extends VerticalLayout {
             doughnutChart.updateChart(productTypes, productCounts);
         } else {
             // Clear the chart if no inventory is selected
-            doughnutChart.updateChart(new String[]{}, new int[]{});
+            doughnutChart.updateChart(new String[]{"No Data"}, new int[]{1});
         }
     }
 }
