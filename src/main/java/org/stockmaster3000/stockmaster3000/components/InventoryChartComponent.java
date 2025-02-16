473ba44ee7e3caeb9ba765e7ad0f5e7dd1d842cd
viettranni <vttranviett@gmail.com>
2025-02-16 20:28:38 +0200

    Fixed the bug with inventory chart when the inventory has no products. Before it didnt display the chart at all when the inventory was empty but now it does

--- src/main/java/org/stockmaster3000/stockmaster3000/components/InventoryChartComponent.java
+++ src/main/java/org/stockmaster3000/stockmaster3000/components/InventoryChartComponent.java
@@ -14,14 +14,12 @@ import com.vaadin.flow.component.html.H3;
 public class InventoryChartComponent extends VerticalLayout {
 
     private final ProductService productService;
-    private final InventoryService inventoryService;
     private DoughnutChart doughnutChart;
 
     // Component Constructor
     // ----------------------------------------------------------------------------------------------------------------------------------------------------------
-    public InventoryChartComponent(SecurityService securityService, ProductService productService, InventoryService inventoryService) {
+    public InventoryChartComponent(SecurityService securityService, ProductService productService) {
         this.productService = productService;
-        this.inventoryService = inventoryService;
 
         H1 componentTopic = new H1("Inventory Insights");
         add(componentTopic);
@@ -42,20 +40,26 @@ public class InventoryChartComponent extends VerticalLayout {
             // Fetch data for the selected inventory
             Map<Category, Integer> productData = productService.getProductDataByInventory(selectedInventory.getId());
     
-            String[] productTypes = new String[productData.size()];
-            int[] productCounts = new int[productData.size()];
-            int index = 0;
+            if (productData.isEmpty()) {
+                // If there are no products, display "No Data"
+                doughnutChart.updateChart(new String[]{"No Data"}, new int[]{1});
+            } else {
+                // Process and display the product data
+                String[] productTypes = new String[productData.size()];
+                int[] productCounts = new int[productData.size()];
+                int index = 0;
     
-            for (Map.Entry<Category, Integer> entry : productData.entrySet()) {
-                productTypes[index] = entry.getKey().getName();
-                productCounts[index] = entry.getValue();
-                index++;
+                for (Map.Entry<Category, Integer> entry : productData.entrySet()) {
+                    productTypes[index] = entry.getKey().getName();
+                    productCounts[index] = entry.getValue();
+                    index++;
+                }
+    
+                // Update the doughnut chart with the new data
+                doughnutChart.updateChart(productTypes, productCounts);
             }
-
-            // Update the doughnut chart with the new data
-            doughnutChart.updateChart(productTypes, productCounts);
         } else {
-            // Clear the chart if no inventory is selected
+            // If no inventory is selected, clear the chart
             doughnutChart.updateChart(new String[]{"No Data"}, new int[]{1});
         }
     }
