653ba1ca163f899f9866d2b973be535745bf68dc
Saed Abukar <saed.a.abukar@gmail.com>
2025-02-26 13:57:25 +0200

    Open api added from viet2 branch

---
+++ src/main/frontend/generated/flow/chunks/chunk-ad9fb9f1d4d0c583af830269d082365b88f82750c53c23ee16ca586a72b72a83.js
@@ -0,0 +1,6 @@
+import '@vaadin/tooltip/theme/lumo/vaadin-tooltip.js';
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/button/theme/lumo/vaadin-button.js';
+import '@vaadin/vertical-layout/theme/lumo/vaadin-vertical-layout.js';
+import '@vaadin/horizontal-layout/theme/lumo/vaadin-horizontal-layout.js';
+import 'Frontend/generated/jar-resources/disableOnClickFunctions.js';
