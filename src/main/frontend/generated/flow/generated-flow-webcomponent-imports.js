a053706141aff28a1e69ef9c96215a4f5248a944
BudaOP <ibudanov24@gmail.com>
2025-03-03 17:34:05 +0200

    Jenkins Docker env file

--- src/main/frontend/generated/flow/generated-flow-webcomponent-imports.js
+++ src/main/frontend/generated/flow/generated-flow-webcomponent-imports.js
@@ -15,14 +15,14 @@ import 'Frontend/generated/jar-resources/ReactRouterOutletElement.tsx';
 
 const loadOnDemand = (key) => {
   const pending = [];
-  if (key === 'ca9bd51a380521522accefd702825df3f99187565565f7dfc924892bd962e067') {
-    pending.push(import('./chunks/chunk-f993328803ee7ddcfbe2b28fd13c514af764642aa2628dc06c4e42d483d0b7f3.js'));
+  if (key === '046d813b669ff911d58ce4f124427dc919a7d904adb8abc8a2cb573b8a7e0315') {
+    pending.push(import('./chunks/chunk-eb52879258ba5d4e4eb49390beddb09f0bc7fb2335334ebd0e4ea9bd558b3ea9.js'));
   }
   if (key === 'a1937aa60de8769fb8cf511465edecca24a6ac3cd35d0ae1045169296020a920') {
     pending.push(import('./chunks/chunk-ad9fb9f1d4d0c583af830269d082365b88f82750c53c23ee16ca586a72b72a83.js'));
   }
-  if (key === '046d813b669ff911d58ce4f124427dc919a7d904adb8abc8a2cb573b8a7e0315') {
-    pending.push(import('./chunks/chunk-eb52879258ba5d4e4eb49390beddb09f0bc7fb2335334ebd0e4ea9bd558b3ea9.js'));
+  if (key === 'ca9bd51a380521522accefd702825df3f99187565565f7dfc924892bd962e067') {
+    pending.push(import('./chunks/chunk-f993328803ee7ddcfbe2b28fd13c514af764642aa2628dc06c4e42d483d0b7f3.js'));
   }
   return Promise.all(pending);
 }
